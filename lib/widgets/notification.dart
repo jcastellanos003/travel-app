8ad4e29d448cf88e8bfbb5c2232f63c033130c8a
lindajess <lgutierrez@buildingengines.com>
2020-08-13 18:23:40 -0500

    remove notification widget

--- lib/widgets/notification.dart
+++
@@ -1,6 +0,0 @@
-//https://medium.com/flutter-community/flutter-notifications-bubble-up-and-values-go-down-c1a499d22d5f
-
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-
-class NotificationGeneric {}
